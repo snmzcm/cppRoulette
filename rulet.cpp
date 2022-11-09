e38de68c840cd748312726b5ef9e2bc44fbdc348
snmzcm <snmzcm@gmail.com>
2022-11-10 02:00:12 +0300

    Fixed some stupidity of mine. Sometimes I forget that is not python I am dealing with.

--- rulet.cpp
+++ rulet.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+//include filesystem and replace remove() with filesystem libs remove function
 
 using namespace std;
 
 int main(){
     int minNumber = 1, maxNumber = 6;
     int possibility,chamberNumber;
-    int del = remove("delete.txt");
     srand(time(0));
     possibility = rand() % (maxNumber - minNumber + 1 ) + minNumber;
     chamberNumber = rand() % (maxNumber - minNumber + 1 ) + minNumber;
     cout << "First Number: " <<possibility<<endl<<"Second Number: " << chamberNumber<< endl;
 
     if (possibility == chamberNumber){
+        remove("C:\\Users\\alice\\Desktop\\cppRoulette\\delete.txt"); 
         cout << "You're Dead " <<possibility<< endl;
-        //Burası şart sağlanmasa dahi mutlaka bir kere çalışıyor.
-        //remove("C:\\Users\\alice\\Desktop\\cppRoulette\\delete.txt"); 
         
     }
-    else{
-    cout << possibility << endl;
-    }
+    // else{
+    // cout << possibility << endl;
+    // }
     
  
 
